2ba6422861963aa4156b205a257df95658c61beb
prashant kumar <00udemy00@gmail.com>
2024-04-02 19:22:49 +0530

    chat design added

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,10 @@
 const sequelize= require("../util/database");
 const user=require("../model/user");
 const bcrypt=require("bcrypt");
+const jwt = require("jsonwebtoken")
+function generateAccessToken(id, email) {
+  return jwt.sign({ userId: id, email: email }, "secretKey");
+}
 module.exports.signup= async(req,res,next)=>{
     const t = await sequelize.transaction();
     const {name,email,password}=req.body;
@@ -32,4 +36,42 @@ module.exports.signup= async(req,res,next)=>{
 
 
 
+}
+
+module.exports.postUserLogin= async function postUserLogin(req, res, next) {
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  try {
+    const user1 = await user.findOne({ where: { Email: email } });
+
+    if (!user1) {
+      return res.status(404).json({
+        success: false,
+        message: "User doesn't Exists!",
+      });
+    }
+
+    bcrypt.compare(password, user1.Password, async (err, result) => {
+      if (err) {
+        return res.status(500).json({ success: false, message: "Something went Wrong!" });
+      }
+
+      if (result == true) {
+        return res.status(200).json({
+          success: true,
+          message: "Login Successful!",
+          token: generateAccessToken(user1.Id, user1.Email)
+        });
+      } else {
+        return res.status(401).json({
+          success: false,
+          message: "Password Incorrect!",
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: "Something went Wrong!" });
+  }
 }
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,5 +1,6 @@
 const express= require("express");
-const signup=require("../controller/user");
+const user=require("../controller/user");
 const router=express.Router();
-router.post("/signup", signup.signup);
+router.post("/signup", user.signup);
+router.post("/login",user.postUserLogin)
 module.exports=router
