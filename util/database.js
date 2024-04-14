39ac2f985a67b3b7664af985ea92f10e918ccde6
prashant kumar <00udemy00@gmail.com>
2024-04-14 16:27:29 +0530

    edit database

--- util/database.js
+++ util/database.js
@@ -1,7 +1,14 @@
 const Sequelize=require("sequelize")
-const sequelize= new Sequelize("chatapp", "root" , "123456",{
-    host:"localhost",
-    port:3306,
+// const sequelize= new Sequelize("chatapp", "root" , "123456",{
+//     host:"localhost",
+//     port:3306,
+//     dialect: 'mysql',
+    
+// })
+console.log(process.env.database)
+const sequelize= new Sequelize(process.env.database, process.env.userName , process.env.password,{
+    host:process.env.host,
+    port:process.env.port,
     dialect: 'mysql',
     
 })
